2f8960ca3d9648f93810d06caa3a12b78235abba
Nguyen Van Khoe <nguyenkhoe2k4@gmail.com>
2024-11-12 18:31:06 +0700

    create order

--- src/components/test/index.js
+++ src/components/test/index.js
@@ -1,64 +1,144 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect, useCallback,useContext } from "react";
 import '../test/index.css';
+import { AuthContext } from "../../components/introduce/AuthContext";
+import debounce from "lodash.debounce";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 
-const initialOrders = [
-  { id: 1532, client: "John Carter", email: "hello@johncarter.com", date: "2024-01-30", status: "Delivered", country: "United States", total: 1099.24 },
-  { id: 1531, client: "Sophie Moore", email: "contact@sophiemoore.com", date: "2024-01-27", status: "Canceled", country: "United Kingdom", total: 5870.32 },
-  { id: 1530, client: "Matt Cannon", email: "info@mattcannon.com", date: "2024-01-24", status: "Delivered", country: "Australia", total: 13899.48 },
-  { id: 1529, client: "Graham Hills", email: "hi@grahamhills.com", date: "2024-01-21", status: "Pending", country: "India", total: 1569.12 },
-  { id: 1528, client: "Sandy Houston", email: "contact@sandyhouston.com", date: "2024-01-18", status: "Delivered", country: "Canada", total: 899.16 },
-  { id: 1527, client: "Andy Smith", email: "hello@andysmith.com", date: "2024-01-15", status: "Pending", country: "United States", total: 2449.64 },
-];
-
-const OrderManagement = () => {
-  const [orders, setOrders] = useState(initialOrders);
+const OrderManagement = ({ onCreateOrder, onHistory,openModalDetail,setIdOrder }) => {
+  const [orders, setOrders] = useState([]);
   const [editingIndex, setEditingIndex] = useState(null);
   const [editedOrder, setEditedOrder] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
+  const [noteDetail, setNoteDetail] = useState(null); // Thay đổi state để theo dõi chỉ số đơn hàng đang chỉnh sửa
+  const {user} = useContext(AuthContext)
+  const createorder = (order) => {
+    return {
+      id: order._id,
+      client: order.nameSupplier,
+      email: order.emailSupplier,
+      status: order.generalStatus,
+      date: order.updatedAt,
+      country: 'vn',
+      total: order.amount,
+      notes: order.notes || '', // Giả sử có trường "notes" trong dữ liệu đơn hàng
+    };
+  };
+  const handleSaveClick = () => {
+    const updatedOrders = [...orders];
+    const newOrder = {...editedOrder,userid:user._id,userName:user.name}
+    newOrder.date = new Date().toISOString();
+    console.log(newOrder.date)
+    updatedOrders[editingIndex] = editedOrder;
+    console.log(newOrder)
+    updateData(newOrder)
+    setOrders(updatedOrders);
+    setEditingIndex(null); 
+    setNoteDetail(null);  
+  };
+
+  const fetchOrder = async (keyword,hrefLink) => {
+    try {
+      const apiUrl = `http://localhost:5000/import/orderHistory/getOrder?search=${keyword}&ownerId=${user.id_owner}&userId=${user._id}`;
+      const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch the order');
+      }
+
+      const data = await response.json();
+      const regurlizationData = data.map(item => createorder(item));
+      setOrders((prev)=>{
+        const newData = [...regurlizationData]
+        console.log(newData)
+        return newData;
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  const updateData = async ( newData) => {
+    try {
+      newData.ownerId= user.id_owner;
+      const response = await fetch(`http://localhost:5000/import/orderHistory/updateOrderhistory`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify(newData),
+      });
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const data = await response.json(); // Đọc phản hồi trả về
+      console.log('Update successful:', data);
+    } catch (error) {
+      console.error('Error during update:', error);
+    }
+  };
+  const debouncedFetchSuggestions = useCallback(
+    debounce((keyword, hrefLink) => {
+      fetchOrder(keyword, hrefLink);
+    }, 500),
+    []
+  );
+  useEffect(() => {
+    fetchOrder();
+  }, []);
+  useEffect(() => {
+    debouncedFetchSuggestions(searchTerm.trim());
+  }, [searchTerm]);  
+
+  const handleCancelClick = () => {
+    setEditingIndex(null); // Hủy chế độ chỉnh sửa
+    setNoteDetail(null);  // Ẩn ghi chú khi hủy
+  };
+
   const handleEditClick = (index, order) => {
     setEditingIndex(index);
     setEditedOrder({ ...order });
-  };
-  let count=0;
-  const handleSaveClick = () => {
-    const updatedOrders = [...orders];
-    updatedOrders[editingIndex] = editedOrder;
-    setOrders(updatedOrders);
-    setEditingIndex(null); // Thoát khỏi chế độ chỉnh sửa
-  };
-
-  const handleCancelClick = () => {
-    setEditingIndex(null); // Hủy chỉnh sửa
+    setNoteDetail(index); // Hiển thị ghi chú khi nhấn "Edit" vào đơn hàng cụ thể
   };
 
   const handleEditChange = (e) => {
     const { name, value } = e.target;
     setEditedOrder(prevOrder => ({ ...prevOrder, [name]: value }));
   };
-  const filteredOrders = orders.filter(order => 
-    order.client.toLowerCase().includes(searchTerm) ||
-    order.email.toLowerCase().includes(searchTerm) ||
-    order.country.toLowerCase().includes(searchTerm)
-  );
-
 
   const handleSearch = (e) => {
-    setSearchTerm(e.target.value.toLowerCase());
+    setSearchTerm(e.target.value);
+  };
+  const transfer = (date) => {
+    const date2 = new Date(date);
+    if (isNaN(date2)) {
+      return 'Invalid date';
+    }
+    return date2.toLocaleString("vi-VN", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    });
   };
   return (
     <div className="order-mgmt-container">
       <div className="order-mgmt-header">
         <h2 className="order-mgmt-title">Quản lý đơn hàng</h2>
         <div className="order-mgmt-header-controls">
-        <input
+          <input
             type="text"
             className="order-mgmt-search"
             placeholder="Search for..."
             value={searchTerm}
             onChange={handleSearch}
           />
-          <button className="order-mgmt-create-btn">Create Order</button>
-          <button className="order-mgmt-history-btn">Lịch sử</button>
+          <button className="order-mgmt-create-btn" onClick={onCreateOrder}>Create Order</button>
+          <button className="order-mgmt-history-btn" onClick={onHistory}>Lịch sử</button>
         </div>
       </div>
 
@@ -74,101 +154,125 @@ const OrderManagement = () => {
             <th>Actions</th>
           </tr>
         </thead>
-        <tbody>
-          {filteredOrders.map((order, index) => (
-            <tr key={order.id}>
-              <td>#{order.id}</td>
-              <td>
-                {editingIndex === index ? (
-                 <div>
-                 <input
-                   type="text"
-                   name="client"
-                   value={editedOrder.client}
-                   onChange={handleEditChange}
-                 />
-                 <input
-                   type="email"
-                   name="email"
-                   value={editedOrder.email}
-                   onChange={handleEditChange}
-                 />
-               </div>
-             ) : (
-               <div>
-                 {order.client} <br />
-                 <small>{order.email}</small>
-               </div>
-                )}
-              </td>
-              <td>
-                {editingIndex === index ? (
-                  <input
-                    type="date"
-                    name="date"
-                    value={editedOrder.date}
-                    onChange={handleEditChange}
-                  />
-                ) : (
-                  order.date
-                )}
-              </td>
-              <td>
-                {editingIndex === index ? (
-                  <select
-                    name="status"
-                    value={editedOrder.status}
-                    onChange={handleEditChange}
-                  >
-                    <option value="Delivered">Delivered</option>
-                    <option value="Pending">Pending</option>
-                    <option value="Canceled">Canceled</option>
-                  </select>
-                ) : (
-                  <span className={`order-mgmt-status ${order.status.toLowerCase()}`}>
-                    {order.status}
-                  </span>
-                )}
-              </td>
-              <td>
-                {editingIndex === index ? (
-                  <input
-                    type="text"
-                    name="country"
-                    value={editedOrder.country}
-                    onChange={handleEditChange}
-                  />
-                ) : (
-                  order.country
-                )}
-              </td>
-              <td>
-                {editingIndex === index ? (
-                  <input
-                    type="String"
-                    name="total"
-                    value={editedOrder.total}
-                    onChange={handleEditChange}
-                    step="0.01"
-                  />
-                ) : (
-                  `$${order.total}`
-                )}
-              </td>
-              <td>
-                {editingIndex === index ? (
-                  <>
-                    <button className="order-mgmt-button save" onClick={handleSaveClick}>Save</button>
-                    <button className="order-mgmt-button cancel" onClick={handleCancelClick}>Cancel</button>
-                  </>
-                ) : (
-                  <>
-                    <button className="order-mgmt-button edit" onClick={() => handleEditClick(index, order)}>✏️</button>
-                    <button className="order-mgmt-button delete">🗑️</button>
-                  </>
-                )}
-              </td>
-            </tr>
+        <tbody >
+          {orders.map((order, index) => (
+            <React.Fragment key={order.id}>
+              <tr >
+                <td>#{order.id}</td>
+                <td>
+                  {editingIndex === index ? (
+                    <div>
+                      <input
+                        type="text"
+                        name="client"
+                        value={editedOrder.client}
+                        onChange={handleEditChange}
+                      />
+                      <input
+                        type="email"
+                        name="email"
+                        value={editedOrder.email}
+                        onChange={handleEditChange}
+                      />
+                    </div>
+                  ) : (
+                    <div>
+                      {order.client} <br />
+                      <small>{order.email}</small>
+                    </div>
+                  )}
+                </td>
+                <td>
+                  {editingIndex === index ? (
+                    <input
+                      type="date"
+                      name="date"
+                      value={editedOrder.date}
+                      onChange={handleEditChange}
+                    />
+                  ) : (
+                    transfer(order.date)
+                  )}
+                </td>
+                <td>
+                  {editingIndex === index ? (
+                    <select
+                      name="status"
+                      value={editedOrder.status}
+                      onChange={handleEditChange}
+                    >
+                      <option value="deliveried">Deliveried</option>
+                      <option value="Pending">Pending</option>
+                      <option value="Canceled">Canceled</option>
+                    </select>
+                  ) : (
+                    <span className={`order-mgmt-status ${order.status.toLowerCase()}`}>
+                      {order.status}
+                    </span>
+                  )}
+                </td>
+                <td>
+                  {editingIndex === index ? (
+                    <input
+                      type="text"
+                      name="country"
+                      value={editedOrder.country}
+                      onChange={handleEditChange}
+                    />
+                  ) : (
+                    order.country
+                  )}
+                </td>
+                <td>
+                  {editingIndex === index ? (
+                    <input
+                      type="number"
+                      name="total"
+                      style={{width:'100%'}}
+                      value={editedOrder.total}
+                      onChange={handleEditChange}
+                      step="0.01"
+                    />
+                  ) : (
+                    `$${order.total}`
+                  )}
+                </td>
+                <td>
+                  {editingIndex === index ? (
+                    <>
+                      <button className="order-mgmt-button save" onClick={handleSaveClick}>Save</button>
+                      <button className="order-mgmt-button cancel" onClick={handleCancelClick}>Cancel</button>
+                    </>
+                  ) : (
+                    <>
+                      <button className="order-mgmt-button edit" onClick={() => handleEditClick(index, order)}>✏️</button>
+                      <FontAwesomeIcon icon={faCircleInfo} onClick={()=>{
+                        openModalDetail();
+                        setIdOrder(order.id)
+                      }}
+                       style={{height: '24px', witdh: '24px', padding:'8px'}} className="infoDetail"/>
+                    </>
+                  )}
+                </td>
+              </tr>
+
+              {/* Render dòng ghi chú dưới mỗi đơn hàng khi nhấn Edit */}
+              {editingIndex === index && noteDetail === index && (
+                <tr>
+                  <td colSpan="7">
+                    <input
+                      style={{outline:'none',border:'none'}}
+                      type="text"
+                      name="notes"
+                      value={editedOrder.notes}
+                      onChange={handleEditChange}
+                      placeholder="Add your notes here"
+                      className="note-input"
+                    />
+                  </td>
+                </tr>
+              )}
+            </React.Fragment>
           ))}
         </tbody>
       </table>
@@ -177,3 +281,4 @@ const OrderManagement = () => {
 };
 
 export default OrderManagement;
+
